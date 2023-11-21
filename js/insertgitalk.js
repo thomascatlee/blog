cc12ff2fc9edd132fc4ac64847b981661186d28f
thomascatlee <thomascatlee@gmail.com>
2023-11-21 14:34:09 +0800

    修正gitalk一直显示不对的问题

--- js/insertgitalk.js
+++ js/insertgitalk.js
@@ -1,25 +1,32 @@
 (function () {
     var myPlugin = function (hook, vm) {
-    	
-    	
-	    hook.afterEach(function(html, next) {
-	        // 解析成 html 后调用。
-	        // beforeEach 和 afterEach 支持处理异步逻辑
-	        // ...
-	        // 异步处理完成后调用 next(html) 返回结果
-	        html = html + "<div id='gitalk-container' style='width: 80%;padding-left: 80px;'></div>";
-	        var config = window.$docsify.gitalk;
-	        var gitalk = new Gitalk({
-			        clientID: config.clientID,
-			        clientSecret: config.clientSecret,
-			        repo: config.repo,
-			        owner: config.owner,
-			        admin: config.admin,
-			        distractionFreeMode: false,
-			        id: md5(location.href.split('#')[1]),
-			    });
-    			gitalk.render('gitalk-container');
-	        next(html);
+
+	    hook.doneEach(function() {
+	    	// Add gitalk container if not exists
+        var previousGitalk = document.getElementById('gitalk-container')
+        if (!previousGitalk) {
+            var gitalkContainer = document.createElement('div')
+            gitalkContainer.id = 'gitalk-container'
+            gitalkContainer.style.maxWidth = '80%'
+            gitalkContainer.style.margin = '0px auto 20px'
+            document.getElementById("main").parentNode.insertBefore(gitalkContainer, undefined)
+        } else {
+ 	
+		        for (var n = previousGitalk; n.hasChildNodes(); )
+		            n.removeChild(n.firstChild);
+        }
+                
+        var config = window.$docsify.gitalk;
+        var gitalk = new Gitalk({
+		        clientID: config.clientID,
+		        clientSecret: config.clientSecret,
+		        repo: config.repo,
+		        owner: config.owner,
+		        admin: config.admin,
+		        distractionFreeMode: false,
+		        id: md5(location.href.split('#')[1]),
+		    });
+  			gitalk.render('gitalk-container');
 	    });
 
     };
